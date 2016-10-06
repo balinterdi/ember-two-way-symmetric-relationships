e75dec5bf6684b8a6a0cb2b5dac2ef9218f91cb7
Balint Erdi <balint.erdi@gmail.com>
2016-10-06 11:34:49 +0200

    Remove code comment

--- app/models/person.js
+++ app/models/person.js
@@ -1,8 +1,6 @@
 import DS from 'ember-data';
 import Model from 'ember-data/model';
 
-//TODO: Fetch not just the friendships but also include the friender and friended relationships
-// along with each friendship (use `include`)
 export default Model.extend({
   name: DS.attr(),
   friendships: DS.hasMany(),
